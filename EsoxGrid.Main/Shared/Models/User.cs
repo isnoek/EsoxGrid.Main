4908bc26bd03d46f471272a34a721a8765e2eabe
Iede Snoek <info@esoxsolutions.nl>
2020-07-04 12:58:27 +0200

    Add project files.

---
+++ EsoxGrid.Main/Shared/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsoxGrid.Main.Shared.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public static IEnumerable<User> Generate(int number)
+        {
+            Random r=new Random();
+            List<User> result=new List<User>();
+            for (int i = 0; i < number; i++)
+            {
+                var user = new User
+                {
+                    Id = i,
+                    Age = r.Next(80),
+                    FirstName = $"First{i}",
+                    LastName = $"Last{i}"
+                };
+                result.Add(user);
+            }
+            return result;
+        }
+    }
+}
